b570a7b695be51538b905636f0c85910dfbde4fe
Jesús Hernández <cjesus02hdz@gmail.com>
2019-02-20 04:53:40 -0600

    CelKel

---
+++ CelKel.py
@@ -0,0 +1,5 @@
+#Convertir grados Celsius a Grados Kelvin
+
+celsius = float(input("Ingresa los grados Celsius que deseas convertir: "))
+
+print((celsius),  "grados Celsius equivale a: ",  str( celsius + 273.15),  "grados Kelvin.")
